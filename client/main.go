fb85ca4d8a27b706456442983b7da90c9ac2b438
surya84 <125242873+surya84@users.noreply.github.com>
2024-02-29 07:54:05 +0530

    Add files via upload

---
+++ client/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+const (
+	port = ":8080"
+)
+
+func main() {
+	conn, err := grpc.Dial("localhost"+port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+
+	if err != nil {
+		log.Fatalf("not connected  : %v", err)
+	}
+
+	defer conn.Close()
+
+	client := pb.
+
+}
